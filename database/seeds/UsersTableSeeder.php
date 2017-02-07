5323576fe55e59dd2d297d5f68d4107348de53d4
Ahsan Ziaullah <ahsanzia17@hotmail.com>
2017-02-07 17:14:54 +0100

    Adding Demo Accounts to users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,12 +12,30 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            [
             'id' => 1,
             'name' => 'admin',
             'email' => 'admin@example.com',
             'password' => bcrypt('password'),
             'email_verified' => '1',
             'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+             [
+            'id' => 2,
+            'name' => 'test1',
+            'email' => 'test1@example.com',
+            'password' => bcrypt('password'),
+            'email_verified' => '1',
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+             [
+            'id' => 3,
+            'name' => 'test2',
+            'email' => 'test2@example.com',
+            'password' => bcrypt('password'),
+            'email_verified' => '1',
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ]
         ]);
     }
 }
