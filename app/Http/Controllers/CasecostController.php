dffd81688566e523652c8815cfa201ec49e370e5
Ahsan Ziaullah <ahsanzia17@hotmail.com>
2017-02-09 16:17:56 +0100

    commit

--- app/Http/Controllers/CasecostController.php
+++ app/Http/Controllers/CasecostController.php
@@ -14,14 +14,12 @@ use App\Casecost;
 
 class CasecostController extends Controller
 {
-      public function getIndex()
+    public function getIndex()
     {
-        $costs = Casecost::all();
-
-        return response()->success(compact('costs'));
+          $costs = Casecost::all();
+          return response()->success(compact('costs'));
     }
-
-      public function postProfile()
+    public function postProfile()
     {
         $casecost = Casecost::create([
             'director' => Input::get('director'),
@@ -31,14 +29,11 @@ class CasecostController extends Controller
             'asst_admin' => Input::get('asst_admin'),
             'j_admin' => Input::get('j_admin'),
         ]);
-
         return response()->success(compact('casecost'));
     }
-
-
-      public function putCostShow(Request $request)
+    public function putCostShow(Request $request)
     {
-    $clientForm = array_dot(
+        $clientForm = array_dot(
             app('request')->only(
                 'data.director',
                 'data.manager',
@@ -50,9 +45,7 @@ class CasecostController extends Controller
                 'data.id'
             )
         );
-
         $clientid = intval($clientForm['data.id']);
-
         $cientdata = [
             'director' => $clientForm['data.director'],
             'manager' => $clientForm['data.manager'],
@@ -61,65 +54,13 @@ class CasecostController extends Controller
             'asst_admin' => $clientForm['data.asst_admin'],
             'j_admin' => $clientForm['data.j_admin'],
             'active' => $clientForm['data.active'],
-    
         ];
-
         $affectedRows = Casecost::where('id', '=', $clientid)->update($cientdata);
         return response()->success('success');
-   
     }
  public function getCostShow($id)
     {
-        $clients = Casecost::find($id);
-
-        return response()->success($clients);
-    }
-
-
-
-/***
-    public function getClientShow($id)
-    {
-        $clients = Client::find($id);
-
-        return response()->success($clients);
-    }
-
-    
-
-    public function putClientShow(Request $request)
-    {
-        $clientForm = array_dot(
-            app('request')->only(
-                'data.fname',
-                'data.mname',
-                'data.lname',
-                'data.drivinglicence',
-                'data.passport',
-                'data.utilitybill',
-                'data.idcard',
-                'data.dob',
-                'data.ninumber',
-                'data.id'
-            )
-        );
-
-        $clientid = intval($clientForm['data.id']);
-
-        $cientdata = [
-            'fname' => $clientForm['data.fname'],
-            'mname' => $clientForm['data.mname'],
-            'lname' => $clientForm['data.lname'],
-            'drivinglicence' => $clientForm['data.drivinglicence'],
-            'passport' => $clientForm['data.passport'],
-            'idcard' => $clientForm['data.idcard'],
-            'utilitybill' => $clientForm['data.utilitybill'],
-            'dob' => $clientForm['data.dob'],
-            'ninumber' => $clientForm['data.ninumber'],
-        ];
-
-        $affectedRows = Client::where('id', '=', $clientid)->update($cientdata);
-
-        return response()->success('success');
+     $clients = Casecost::find($id);
+     return response()->success($clients);
     }
 }
